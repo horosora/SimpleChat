37ffd4db8481e5d7f439f230aebbc336c3547020
horosora <horosora@gmail.com>
2018-04-26 00:21:54 +0900

    update

--- src/main/java/simplechat/AppController.java
+++ src/main/java/simplechat/AppController.java
@@ -13,14 +13,21 @@ public class AppController {
     @MessageMapping("/endpoint")
     @SendTo("/topic/messages")
     public DataToSend message(DataToRecv redvData) {
-        if (redvData.getName().isEmpty()) {
-            redvData.setName("名無しのスーパーハッカー");
-        }
-
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         String date = now.format(fmt);
 
+        if (redvData.getName().isEmpty()) {
+            redvData.setName("名無しのスーパーハッカー");
+        }
+
+        redvData.setName(EscapeProcessing(redvData.getName()));
+        redvData.setMessage(EscapeProcessing(redvData.getMessage()));
+
         return new DataToSend(redvData.getName(), redvData.getMessage(), date);
+    }
+
+    public String EscapeProcessing(String str) {
+        return str.replace("&", "&amp;").replace("\"", "&quot;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&#39;");
     }
 }
